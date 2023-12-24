6574247d614154ca75ee801db29b8720fac9ab56
jan.koester <jan.koester@tuxist.de>
2023-12-24 07:31:31 +0100

    libsecure has now find support

---
+++ src/libsecureidConfig.cmake
@@ -0,0 +1,4 @@
+include(CMakeFindDependencyMacro)
+# find_dependency(xx 2.0)
+include(${CMAKE_CURRENT_LIST_DIR}/libsecureidTargets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/libsecureidVersion.cmake)
